516952c988a2e8610328ad2abba91c03f8b1262e
pabitra-at-414227014137 <mailtopabi@gmail.com>
2025-02-26 00:41:50 +0530

    updated test case

--- DSA/1d array/merge-array/test_merge_sorted_array.py
+++ DSA/1d array/merge-array/test_merge_sorted_array.py
@@ -4,7 +4,10 @@ from merge_sorted_array import MergeArray
 @pytest.mark.parametrize(
     "nums1, m, nums2, n, expected",
     [
-        ([2, 7, 0, 0], 2, [1, 6], 2, [1, 2, 6, 7])
+        ([2, 7, 0, 0], 2, [1, 6], 2, [1, 2, 6, 7]),
+        ([1,2,3,0,0,0], 3, [2,5,6], 3, [1,2,2,3,5,6]),
+        ([1], 1, [], 0, [1]),
+        ([0], 0, [1], 1, [1])
     ]
 )
 def test_merge(nums1, m, nums2, n, expected):
