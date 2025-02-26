20e0ed998f5ba6007213b7140ad5393ec3a23e8d
pabitra-at-414227014137 <mailtopabi@gmail.com>
2025-02-26 20:05:14 +0530

    added test case

--- DSA/1d array/array-to-bst/test_array_to_bst.py
+++ DSA/1d array/array-to-bst/test_array_to_bst.py
@@ -1,12 +1,42 @@
 import pytest
-from array_to_bst import ArrayToBst
+from array_to_bst import ArrayToBst, TreeNode
 
 @pytest.mark.parametrize(
-    "nums, expected",
+    "nums",
     [
-        
+        ([-10,-3,0,5,9])
     ]
 )
-def test_merge(nums, expected):
+def test_merge(nums):
     actual = ArrayToBst().sortedArrayToBST(nums)
-    assert  actual == expected
+    final_result = []
+    assert inorder_traversal(actual, final_result) == nums
+    #assert is_balanced(actual) == True
+
+def inorder_traversal(root: TreeNode, result: list[int]):
+    if root:
+        inorder_traversal(root.left, result)
+        result.append(root.val)
+        inorder_traversal(root.right, result)
+
+def check_height(root):
+    """Returns the height of the tree if balanced, otherwise -1."""
+    if root is None:
+        return 0  # Base case: Height of an empty tree is 0
+
+    left_height = check_height(root.left)
+    if left_height == -1:
+        return -1  # Left subtree is unbalanced
+    
+    right_height = check_height(root.right)
+    if right_height == -1:
+        return -1  # Right subtree is unbalanced
+    
+    if abs(left_height - right_height) > 1:
+        return -1  # If height difference > 1, tree is unbalanced
+
+    return max(left_height, right_height) + 1  # Return height of current node
+
+def is_balanced(root: TreeNode) -> bool:
+    """Returns True if the BST is balanced, otherwise False."""
+    return check_height(root) != -1
