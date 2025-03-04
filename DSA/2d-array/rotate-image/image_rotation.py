c31297c1f2c68004dd5c98754fa11cd9031f4d6d
Pabitra Muni <pabitra.muni@solugenix.com>
2025-03-04 17:31:17 +0530

    image rotation

--- DSA/2d-array/rotate-image/image_rotation.py
+++ DSA/2d-array/rotate-image/image_rotation.py
@@ -4,29 +4,36 @@ class ImageRotation:
     def rotate(self, matrix: list[list[int]]):
         n = len(matrix)
         if n > 1:
-            for r in range(n-1):
-              temp = matrix[0][0]
-              i = 1
-              while i <= n-1:
-                  matrix[i-1][0] = matrix[i][0] # PUSH UP
-                  i += 1
+            left = 0
+            right = n-1
+            while left < right:
+              for r in range(n-1):
+                temp = matrix[left][left]
+                i = left + 1
+                while i <= right:
+                    matrix[i-1][left] = matrix[i][left] # PUSH UP
+                    i += 1
 
-              i = 1
-              while i <= n-1:
-                  matrix[n-1][i-1] = matrix[n-1][i] # PUSH LEFT
-                  i += 1
+                i = left + 1
+                while i <= right:
+                    matrix[right][i-1] = matrix[right][i] # PUSH LEFT
+                    i += 1
 
-              i = n-2
-              while i >= 0:
-                  matrix[i+1][n-1] = matrix[i][n-1] # PUSH DOWN
-                  i -= 1
+                i = right - 1
+                while i >= 0:
+                    matrix[i+1][right] = matrix[i][right] # PUSH DOWN
+                    i -= 1
 
-              i = n-2
-              while i > 0:
-                  matrix[0][i+1] = matrix[0][i] # PUSH RIGHT
-                  i -= 1
+                i = right - 1
+                while i > 0:
+                    matrix[left][i+1] = matrix[left][i] # PUSH RIGHT
+                    i -= 1
 
-              matrix[0][1] = temp
+                matrix[left][left+1] = temp
+
+              left += 1
+              right -= 1
+              n -= 2
 
 
 
