d3161404e8aa4849e6bf901a0322f2072f458314
Pabitra Muni <pabitra.muni@solugenix.com>
2025-03-04 18:25:54 +0530

    image rotation

--- DSA/2d-array/rotate-image/image_rotation.py
+++ DSA/2d-array/rotate-image/image_rotation.py
@@ -1,38 +1,46 @@
 # https://leetcode.com/problems/rotate-image/?envType=problem-list-v2&envId=array
 
+"""
+the idea is to rotate the outer boundary first i.e. (0, 0) and (n-1, n-1)
+then move to the next inner boundary and rotate it i.e. (1, 1) and (n-2, n-2)
+....
+....
+keep moving the inner boundaries and rotate them until size=2
+"""
+
 class ImageRotation:
     def rotate(self, matrix: list[list[int]]):
         n = len(matrix)
         if n > 1:
-            left = 0
-            right = n-1
-            while left < right:
-              for r in range(n-1):
-                temp = matrix[left][left]
-                i = left + 1
-                while i <= right:
-                    matrix[i-1][left] = matrix[i][left] # PUSH UP
+            x = 0
+            y = n-1
+            while x < y:
+              for rotation in range(n-1):
+                temp = matrix[x][x]
+                i = x + 1
+                while i <= y:
+                    matrix[i-1][x] = matrix[i][x] # PUSH UP
                     i += 1
 
-                i = left + 1
-                while i <= right:
-                    matrix[right][i-1] = matrix[right][i] # PUSH LEFT
+                i = x + 1
+                while i <= y:
+                    matrix[y][i-1] = matrix[y][i] # PUSH LEFT
                     i += 1
 
-                i = right - 1
-                while i >= 0:
-                    matrix[i+1][right] = matrix[i][right] # PUSH DOWN
+                i = y - 1
+                while i >= x:
+                    matrix[i+1][y] = matrix[i][y] # PUSH DOWN
                     i -= 1
 
-                i = right - 1
-                while i > 0:
-                    matrix[left][i+1] = matrix[left][i] # PUSH RIGHT
+                i = y - 1
+                while i > x:
+                    matrix[x][i+1] = matrix[x][i] # PUSH RIGHT
                     i -= 1
 
-                matrix[left][left+1] = temp
+                matrix[x][x+1] = temp
 
-              left += 1
-              right -= 1
+              x += 1
+              y -= 1
               n -= 2
 
 
